cde93f80f988a7875599dc750c4cfe81d3468243
chaynsaw <chaynsaw@gmail.com>
2019-05-01 19:58:23 -0700

    Implement database schema/tables/client

--- server/schema.sql
+++ server/schema.sql
@@ -1,13 +1,54 @@
+DROP DATABASE IF EXISTS chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
-);
+-- CREATE TABLE messages (
+--   id INT AUTO_INCREMENT PRIMARY KEY,
+--   msg VARCHAR(255),
+--   room_id 
+-- );
 
 /* Create other tables and define schemas for them here! */
 
+DROP TABLE IF EXISTS `rooms`;
+		
+CREATE TABLE `rooms` (
+  `id` INTEGER AUTO_INCREMENT,
+  `roomname` VARCHAR(50),
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'users'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `users`;
+		
+CREATE TABLE `users` (
+  `id` INTEGER AUTO_INCREMENT,
+  `username` VARCHAR(15),
+  PRIMARY KEY (`id`),
+KEY (`username`, `id`)
+);
+
+-- ---
+-- Table 'messages'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `messages`;
+		
+CREATE TABLE `messages` (
+  `id` INTEGER AUTO_INCREMENT,
+  `text` VARCHAR(255),
+  `room_id` INTEGER,
+  `user_id` INTEGER(25),
+  `created_at` TIMESTAMP,
+  PRIMARY KEY (`id`)
+);
 
 
 
